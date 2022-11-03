b7f46959e61d1142993b1cb15fea3e172ab3968d
Suprenovah <jtshacket@gmail.com>
2022-11-02 21:23:25 -0400

    Week 9

---
+++ Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public GameObject blackBallPrefab;
+    public GameObject plane;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        if (OtherBall.GetComponent<RedBallCollisionManager>() != null)
+        {
+            Debug.Log("Green Collided With Red");
+
+            Instantiate(blackBallPrefab, plane.transform);
+        }
+    }
+}
+
---
+++ Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public GameObject blackBallPrefab;
+    public GameObject plane;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        if (OtherBall.GetComponent<BlueBallCollisionManager>() != null)
+        {
+            Debug.Log("Red Collided With Blue");
+
+            Instantiate(blackBallPrefab, plane.transform);
+        }
+
+        if (OtherBall.GetComponent<GreenBallCollisionManager>() != null)
+        {
+            Debug.Log("Green Collided With Red");
+        }
+    }
+}
+
